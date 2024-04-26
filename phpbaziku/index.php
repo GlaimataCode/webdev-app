530c35e1847d1cd515cdb6b687abe04afd2093d6
GlaimataCode <151416502+GlaimataCode@users.noreply.github.com>
2024-04-26 12:49:03 +0900

    Uploaded All
    Uploaded All

---
+++ phpbaziku/index.php
@@ -0,0 +1,32 @@
+<?php 
+// ida ne'e komentariu single Comment
+/* ne mos komentariu| Multi Comment*/
+
+//Standard Output
+//echo, print, prin_r, var_dum
+//echo "Hello, Echo! | ";
+//print("Hello, Print! | ");
+//print_r("Hello, Print R | ");
+//var_dump('Hello, Var Dump | '); //debugin
+
+//tipu Data
+// integer, float, String no Boolean
+// String = '' / ""
+//Boolean = true / false /
+//          1 / 0
+//var_dump(true);
+//variavel - Fatin
+// $koko
+$test ="Matcho Elmart";
+
+echo $test;
+
+//oprators
+//aritmatika
+//concatanation
+//comparison
+//logika
+//identity
+
+//looping
+//System Logic / If Else
